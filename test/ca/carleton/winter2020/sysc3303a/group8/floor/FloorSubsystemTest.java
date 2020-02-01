e8ee7a1d2e5620f83dc62dbaa98429523a85907a
Frank Xu <github.com@akaza.red>
2020-01-31 22:22:26 -0500

    Packaging changes

--- test/ca/carleton/winter2020/sysc3303a/group8/floor/FloorSubsystemTest.java
+++ test/ca/carleton/winter2020/sysc3303a/group8/floor/FloorSubsystemTest.java
@@ -1,10 +1,10 @@
-package ca.carleton.winter2020.sysc3303a.group8.elevator;
+package ca.carleton.winter2020.sysc3303a.group8.floor;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class SchedulerTest {
+class FloorSubsystemTest {
 
     @Test
     void test() {
--- test/ca/carleton/winter2020/sysc3303a/group8/scheduler/SchedulerTest.java
+++ test/ca/carleton/winter2020/sysc3303a/group8/scheduler/SchedulerTest.java
@@ -1,10 +1,10 @@
-package ca.carleton.winter2020.sysc3303a.group8.elevator;
+package ca.carleton.winter2020.sysc3303a.group8.scheduler;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class FloorSubsystemTest {
+class SchedulerTest {
 
     @Test
     void test() {
