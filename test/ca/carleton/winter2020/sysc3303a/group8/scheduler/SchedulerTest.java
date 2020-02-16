5392422990e5dd2d5a9de097a2e12710bd421fd7
XuchengShi0 <55570235+XuchengShi0@users.noreply.github.com>
2020-02-15 22:13:16 -0500

    Update SchedulerTest.java

--- test/ca/carleton/winter2020/sysc3303a/group8/scheduler/SchedulerTest.java
+++ test/ca/carleton/winter2020/sysc3303a/group8/scheduler/SchedulerTest.java
@@ -5,10 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class SchedulerTest {
-
+    private Scheduler s;
+    
+    public void setUp() throws Exception {
+		s = new Scheduler(1);
+	}
+    public void tearDown() throws Exception {
+		s = null;
+	}
+    
     @Test
     void test() {
-        fail("Not yet implemented");
+        assertEquals(scheduler.SchedulerStates.WAITING, s.getStates());
+        s.receiveStop(Direction.UP, 2);
+        assertEquals(scheduler.SchedulerStates.R_ELEVATOR, s.getStates());
     }
 
 }
