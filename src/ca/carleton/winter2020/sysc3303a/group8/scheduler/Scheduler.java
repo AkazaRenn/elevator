45015a4f04d6e3a0e7ba55232810a0b9d6737714
XuchengShi0 <55570235+XuchengShi0@users.noreply.github.com>
2020-02-15 22:24:32 -0500

    Update Scheduler.java

--- src/ca/carleton/winter2020/sysc3303a/group8/scheduler/Scheduler.java
+++ src/ca/carleton/winter2020/sysc3303a/group8/scheduler/Scheduler.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import ca.carleton.winter2020.sysc3303a.group8.floor.FloorSubsystem;
 import ca.carleton.winter2020.sysc3303a.group8.utils.Direction;
+import ca.carleton.winter2020.sysc3303a.group8.scheduler.SchedulerStates;
 
 /**
  * Scheduler subsystem of the project.
@@ -19,6 +20,7 @@ public class Scheduler extends Thread {
     public final int MAX_FLOOR = 10;
     public final int MIN_FLOOR = 1;
     public final int DELAY = 250;
+    private SchedulerStates states;
     
     public Scheduler(int elevatorNumber) {
         ELEVATORS = new ArrayList<>(elevatorNumber);
@@ -26,6 +28,7 @@ public class Scheduler extends Thread {
             ELEVATORS.add(new Elevator(this,id,MIN_FLOOR,MAX_FLOOR));
         }
         FLOORSYS = new FloorSubsystem(MIN_FLOOR,MAX_FLOOR);
+        states = SchedulerStates.WAITING;
     }
     
     public void receiveStop(Direction direction, int floor) {
@@ -35,6 +38,7 @@ public class Scheduler extends Thread {
 
     public synchronized void arriveAtFloor(int elevatorId, int floor) {
         ELEVATORS.get(elevatorId).setCurrentFloor(floor);
+        states = SchedulerStates.R_ELEVATOR;
     }
 
     public void addStop(int elevatorId, int floor) {
@@ -51,7 +55,13 @@ public class Scheduler extends Thread {
 //            FLOORSYS.get(i).setDirecionLamp(ELEVATORS.get(elevatorId).getDirection());
 //        }
     }
+    public SchedulerStates getStates(){
+        return states;
+    }
     
+    public void receiveFromFloor(){
+        states = SchedulerStates.R_FLOOR;
+    }
 
     @Override
     public void run() {
