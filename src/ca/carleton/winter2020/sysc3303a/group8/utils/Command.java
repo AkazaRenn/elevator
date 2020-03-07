9885148c53caefb3b46b0a6bf4156a3f4f42208d
XuchengShi0 <55570235+XuchengShi0@users.noreply.github.com>
2020-03-07 15:16:41 -0500

    Update Command.java

--- src/ca/carleton/winter2020/sysc3303a/group8/utils/Command.java
+++ src/ca/carleton/winter2020/sysc3303a/group8/utils/Command.java
@@ -3,7 +3,22 @@ package ca.carleton.winter2020.sysc3303a.group8.utils;
 public enum Command {
     UP((byte)0x01),
     DOWN((byte)0x02),
-    HOLD((byte)0x00);
+    HOLD((byte)0x00),
+    ACK(1),
+    CMD(2),
+    DATA(3),
+    ERROR(0),
+    FLOOR_BUTTON((byte)0x10),
+    ELEVATOR_ARRIVED((byte)0x11),
+    UP_PICKUP((byte)0x33),
+    UP_DROPOFF((byte)0x32),
+    DOWN_PICKUP((byte)0x31),
+    DOWN_DROPOFF((byte)0x30),
+    DOOR_OPEN((byte)0x3A),
+    DOOR_CLOSE((byte)0x3B),
+    STOP((byte)0x3C),
+    ERROR_DOOR_JAM((byte)0xE0),
+    ERROR_STUCK((byte)0xE1);
     
     private byte commandByte;
     
