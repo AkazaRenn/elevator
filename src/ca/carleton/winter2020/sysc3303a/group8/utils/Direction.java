489ba4f72bc298e568ff22268cb3f84dcdafe1a0
Frank Xu <github.com@akaza.red>
2020-03-07 10:23:47 -0500

    Update direction with data

--- src/ca/carleton/winter2020/sysc3303a/group8/utils/Direction.java
+++ src/ca/carleton/winter2020/sysc3303a/group8/utils/Direction.java
@@ -1,7 +1,17 @@
 package ca.carleton.winter2020.sysc3303a.group8.utils;
 
 public enum Direction {
-    UP,
-    DOWN,
-    HOLD;
+    UP((byte)0x00, (byte)0x00, (byte)0x00, (byte)0x01),
+    DOWN((byte)0x00, (byte)0x00, (byte)0x00, (byte)0x02),
+    HOLD((byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00);
+    
+    private byte[] commandBytes;
+    
+    private Direction(byte ... commandBytes) {
+        this.commandBytes = commandBytes;
+    }
+
+    public byte[] getCommandBytes() {
+        return commandBytes;
+    }
 }
