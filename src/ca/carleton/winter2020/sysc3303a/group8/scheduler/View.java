db53200dd7a327d875572bc0335cd91aa2522100
Frank Xu <github.com@akaza.red>
2020-04-05 23:10:21 -0400

    Fix an issue on labelling, add screenshots

--- src/ca/carleton/winter2020/sysc3303a/group8/scheduler/View.java
+++ src/ca/carleton/winter2020/sysc3303a/group8/scheduler/View.java
@@ -12,61 +12,65 @@ import ca.carleton.winter2020.sysc3303a.group8.utils.Utils;
 
 @SuppressWarnings("serial")
 public class View extends JFrame {
-    private JPanel panel;
-    private JTextField[][] floors = new JTextField[Utils.FLOOR_COUNT][Utils.CAR_COUNT];
-    private JTextField[][] directions = new JTextField[2][Utils.CAR_COUNT];
+	private JPanel panel;
+	private JTextField[][] floors = new JTextField[Utils.FLOOR_COUNT][Utils.CAR_COUNT];
+	private JTextField[][] directions = new JTextField[2][Utils.CAR_COUNT];
 
-    public View() {
-        panel = new JPanel();
-        panel.setLayout(new GridLayout(Utils.FLOOR_COUNT + 4, Utils.CAR_COUNT, 20, 0));
+	public View() {
+		panel = new JPanel();
+		panel.setLayout(new GridLayout(Utils.FLOOR_COUNT + 4, Utils.CAR_COUNT, 20, 0));
 
-        for (int i = 0; i < Utils.CAR_COUNT; i++) {
-            panel.add(new JLabel("Car " + (i + 1)));
-        }
+		for (int i = 0; i < Utils.CAR_COUNT; i++) {
+			panel.add(new JLabel("Car " + (i + 1)));
+		}
 
-        for (int j = 0; j < Utils.CAR_COUNT; j++) {
-            directions[0][j] = new JTextField("▲");
-            directions[1][j] = new JTextField("▼");
-            panel.add(directions[0][j]);
-            panel.add(directions[1][j]);
-            directions[0][j].setEnabled(false);
-            directions[1][j].setEnabled(false);
-        }
+		for (int j = 0; j < Utils.CAR_COUNT; j++) {
+			directions[0][j] = new JTextField("▲");
+			directions[1][j] = new JTextField("▼");
+			directions[0][j].setEnabled(false);
+			directions[1][j].setEnabled(false);
+		}
 
-        for (int i = 0; i < Utils.FLOOR_COUNT; i++) {
-            for (int j = 0; j < Utils.CAR_COUNT; j++) {
-                floors[i][j] = new JTextField("" + (Utils.FLOOR_COUNT - i));
-                floors[i][j].setBackground(Color.BLACK);
-                panel.add(floors[i][j]);
-                floors[i][j].setEnabled(false);
-            }
-        }
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < Utils.CAR_COUNT; j++) {
+				panel.add(directions[i][j]);
+			}
+		}
 
-        panel.setVisible(true);
-        panel.setBackground(Color.LIGHT_GRAY);
-        this.add(panel);
+		for (int i = 0; i < Utils.FLOOR_COUNT; i++) {
+			for (int j = 0; j < Utils.CAR_COUNT; j++) {
+				floors[i][j] = new JTextField("" + (Utils.FLOOR_COUNT - i));
+				floors[i][j].setBackground(Color.BLACK);
+				panel.add(floors[i][j]);
+				floors[i][j].setEnabled(false);
+			}
+		}
 
-        this.setVisible(true);
-        this.setTitle("Elevator");
-        this.setSize(400, 750);
-        this.setResizable(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel.setVisible(true);
+		panel.setBackground(Color.LIGHT_GRAY);
+		this.add(panel);
 
-    }
+		this.setVisible(true);
+		this.setTitle("Elevator");
+		this.setSize(400, 750);
+		this.setResizable(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    public JTextField getFloors(int i, int j) {
-        return floors[i][j];
-    }
+	}
 
-    public void setFloors(JTextField[][] floors) {
-        this.floors = floors;
-    }
+	public JTextField getFloors(int i, int j) {
+		return floors[i][j];
+	}
 
-    public JTextField getDirections(int i, int j) {
-        return directions[i][j];
-    }
+	public void setFloors(JTextField[][] floors) {
+		this.floors = floors;
+	}
 
-    public void setDirections(JTextField[][] directions) {
-        this.directions = directions;
-    }
+	public JTextField getDirections(int i, int j) {
+		return directions[i][j];
+	}
+
+	public void setDirections(JTextField[][] directions) {
+		this.directions = directions;
+	}
 }
